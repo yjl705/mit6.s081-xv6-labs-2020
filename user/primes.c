a0fd7dadfe5c4e4733bf11cdc4a098c6563589c3
jialuo.yuan <jialuo.yuan@bytedance.com>
2024-06-17 11:30:15 -0700

    lab1 find

--- user/primes.c
+++ user/primes.c
@@ -15,18 +15,18 @@ void primes(int left[2]){
     pipe(right);
 
     if(fork()==0) {
-        close(left[0]);
-        close(right[1]);
+        close(left[0]); // the read is for parent only, no use in child
+        close(right[1]); // the right is for parent only, no use in child
         primes(right);
     }else{
-        close(right[0]);
+        close(right[0]); // the read is for child only
         int buf;
         while(read(left[0], &buf, sizeof(buf)) != 0) {
             if (buf % p != 0) {
                 write(right[1], &buf, sizeof(buf));
             }
         }
-        close(right[1]);
+        close(right[1]); // end the write
         wait(0);
         exit(0);
     }
@@ -39,12 +39,13 @@ main(int argc, char *argv[])
     int p[2];
     pipe(p);
 
+    // 差别对待要在if分支里做，如果在外面就关闭了某个文件描述符，会导致child也受到同样影响
     if(fork() == 0) {
-        close(p[1]);
+        close(p[1]); // the write is for parent only
         primes(p);
         exit(0);
     } else {
-        close(p[0]);
+        close(p[0]); // the read is for child only
         for(int i = 2; i <= 35; ++i) {
             write(p[1], &i, sizeof(i));
         }
