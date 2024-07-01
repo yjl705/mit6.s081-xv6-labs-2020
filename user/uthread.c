eed771a13e3838058a0b43b6e033f5c5245db193
jialuo.yuan <jialuo.yuan@bytedance.com>
2024-07-01 09:49:47 -0700

    lab7 switch between threads

--- user/uthread.c
+++ user/uthread.c
@@ -11,14 +11,35 @@
 #define MAX_THREAD  4
 
 
+// copied from kernel/proc.h
+// Saved registers for kernel context switches.
+struct context {
+  uint64 ra;
+  uint64 sp;
+
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
+
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
-
+  struct context ctx; // 在 thread 中添加 context 结构体
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
-extern void thread_switch(uint64, uint64);
+extern void thread_switch(struct context*, struct context*);
               
 void 
 thread_init(void)
@@ -63,6 +84,7 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    thread_switch(&t->ctx, &next_thread->ctx);
   } else
     next_thread = 0;
 }
@@ -77,6 +99,10 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  // 参考proc.c
+  t->ctx.ra = (uint64)func; // 返回地址
+
+  t->ctx.sp = (uint64) &t->stack + STACK_SIZE - 1; // 栈指针指在最高位，向低位增长
 }
 
 void 
