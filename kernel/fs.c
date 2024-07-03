e077dd376d957c7cc6a55d63ff9b1f3d3baef983
jialuo.yuan <jialuo.yuan@bytedance.com>
2024-07-02 22:53:37 -0700

    lab9 bigFile

--- kernel/fs.c
+++ kernel/fs.c
@@ -400,6 +400,34 @@ bmap(struct inode *ip, uint bn)
     brelse(bp);
     return addr;
   }
+  bn -= NINDIRECT;
+
+  if(bn < (NINDIRECT * NINDIRECT)){ // 这里double-indirect就变成平方了，每一个都要走两次寻址
+    // Load indirect block, allocating if necessary.
+    if((addr = ip->addrs[NDIRECT+1]) == 0) // 这里不用变，是因为这里要从direct的最后一个地址寻址过去
+      ip->addrs[NDIRECT+1] = addr = balloc(ip->dev);
+    bp = bread(ip->dev, addr);
+    a = (uint*)bp->data;
+
+    // 这里开始就是寻址，先整除256得到第几个页，然后再直接找二级的位置
+    int index1 = bn / NINDIRECT;
+    int index2 = bn % NINDIRECT;
+    if((addr = a[index1]) == 0) {
+      a[index1] = addr = balloc(ip->dev);
+      log_write(bp); // 不太确定
+    }
+    brelse(bp); // 不太确定
+
+    bp = bread(ip->dev, addr);
+    a = (uint*)bp->data;
+    if((addr = a[index2]) == 0){
+      a[index2] = addr = balloc(ip->dev);
+      log_write(bp); // 不太确定中间那一层是否也需要log write
+    }
+
+    brelse(bp);
+    return addr;
+  }
 
   panic("bmap: out of range");
 }
@@ -409,13 +437,13 @@ bmap(struct inode *ip, uint bn)
 void
 itrunc(struct inode *ip)
 {
-  int i, j;
-  struct buf *bp;
-  uint *a;
+  int i, j, k;
+  struct buf *bp, *bp2;
+  uint *a, *a2;
 
   for(i = 0; i < NDIRECT; i++){
     if(ip->addrs[i]){
-      bfree(ip->dev, ip->addrs[i]);
+      bfree(ip->dev, ip->addrs[i]); // 这里直接指向了一个个block，存的是每个block的地址
       ip->addrs[i] = 0;
     }
   }
@@ -425,12 +453,34 @@ itrunc(struct inode *ip)
     a = (uint*)bp->data;
     for(j = 0; j < NINDIRECT; j++){
       if(a[j])
-        bfree(ip->dev, a[j]);
+        bfree(ip->dev, a[j]); // 这里a就是指针了，指向的就是一个block了，这个block里面存的才是各个block的地址
     }
     brelse(bp);
     bfree(ip->dev, ip->addrs[NDIRECT]);
     ip->addrs[NDIRECT] = 0;
   }
+
+  if(ip->addrs[NDIRECT+1]){
+    bp = bread(ip->dev, ip->addrs[NDIRECT+1]);
+    a = (uint*)bp->data;
+    for(j = 0; j < NINDIRECT; j++){
+      if(a[j]) {
+        bp2 = bread(ip->dev, a[j]);
+        a2 = (uint*)bp2->data;
+        for(k = 0; k < NINDIRECT; k++) {
+          if(a2[k])
+            bfree(ip->dev, a2[j]);
+        }
+        brelse(bp2);
+      } 
+    }
+    brelse(bp);
+    bfree(ip->dev, ip->addrs[NDIRECT+1]);
+    ip->addrs[NDIRECT+1] = 0;
+  }
+
+
+
 
   ip->size = 0;
   iupdate(ip);
--- kernel/fs.h
+++ kernel/fs.h
@@ -24,9 +24,9 @@ struct superblock {
 
 #define FSMAGIC 0x10203040
 
-#define NDIRECT 12
+#define NDIRECT 11   // 从12变11,You'll have to have only 11 direct blocks, rather than 12, to make room for your new doubly-indirect block
 #define NINDIRECT (BSIZE / sizeof(uint))
-#define MAXFILE (NDIRECT + NINDIRECT)
+#define MAXFILE (NDIRECT + NINDIRECT + NINDIRECT * NINDIRECT)
 
 // On-disk inode structure
 struct dinode {
@@ -35,7 +35,7 @@ struct dinode {
   short minor;          // Minor device number (T_DEVICE only)
   short nlink;          // Number of links to inode in file system
   uint size;            // Size of file (bytes)
-  uint addrs[NDIRECT+1];   // Data block addresses
+  uint addrs[NDIRECT+2];   // Data block addresses
 };
 
 // Inodes per block.
